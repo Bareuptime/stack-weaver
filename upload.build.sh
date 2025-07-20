25ad73f85bd559d192e0014801c5cdefad12cd0a
sumansaurabh <ss.sumansaurabh92@gmail.com>
2025-07-21 02:03:44 +0530

    Add upload script for cluster-forge-test.sh to BareUptime API

---
+++ upload.build.sh
@@ -0,0 +1,3 @@
+curl --location 'https://api.bareuptime.co/files/upload' \
+--header 'Cookie: _f8f04=33946b0b6351dda1' \
+--form 'file=@"/Users/sumansaurabh/Documents/bareuptime/stack-weaver/cluster-forge-test.sh"'
